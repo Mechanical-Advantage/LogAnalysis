0b655d73314e18441c69e575c25ba5be4ff108f6
Aum Patel <aumpatel130806@gmail.com>
2023-11-02 21:11:39 -0400

    add way to calculate average of voltage values

--- log_analyizer.py
+++ log_analyizer.py
@@ -3,4 +3,7 @@ import pandas as pd
 colnames=['metric', 'data_type', 'value', 'timestamp']
 df = pd.read_csv("../robodatalogs/Log_9d8f62f2e34a918e.gz", quotechar='|', header=None, names=colnames)
 
-print(df)
+df_voltage = df[df['metric'] == '/SystemStats/5vRail/Voltage']
+df_voltage['value'] = pd.to_numeric(df_voltage['value'])
+print(df_voltage['value'].mean())
+print(len(df_voltage.index))
