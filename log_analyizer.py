72dcbc784cb1df359a9add1c403b32b1de767d88
Aum Patel <aumpatel130806@gmail.com>
2023-11-16 21:21:20 -0500

    add columns for values of different data types

--- log_analyizer.py
+++ log_analyizer.py
@@ -21,4 +21,15 @@ if __name__ == "__main__":
     log_metadata = [[log_filename, build_date, robot, runtime_type, git_branch, project_name]]
 
     df_log_metadata = pd.DataFrame(log_metadata, columns=['LogFileName', 'BuildDate', 'Robot', 'RuntimeType', 'GitBranch', 'ProjectName'])
-    print(df_log_metadata)
+
+    df_boolean_log_data = df[df['data_type'] == 'boolean']
+    df_boolean_log_data['boolean_value'] = df_boolean_log_data['value'].astype(bool)
+    df_numerical_log_data = df[(df['data_type'] == 'int64') | (df['data_type'] == 'double') | (df['data_type'] == 'float')]
+    df_numerical_log_data['numeric_value'] = pd.to_numeric(df_numerical_log_data['value'])
+    df_other_log_data = df[(df['data_type'] != 'boolean') & (df['data_type'] != 'int64') & (df['data_type'] != 'double') & (df['data_type'] != 'float')]
+
+    df_log_data = pd.concat([df_boolean_log_data, df_numerical_log_data, df_other_log_data])
+    df_log_data['timestamp'] = df_log_data['timestamp'].astype('int64')
+    df_log_data['log_file_name'] = log_filename
+
+    print(df_log_data)
